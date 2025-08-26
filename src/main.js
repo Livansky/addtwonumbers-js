2d857d397ae256c620d3489025f25105b1647c0a
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-08-26 10:27:09 -0600

    la funcion suma dos numeros e imprime en pantalla el valor

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
-  //aqui va tu código
+export function addTwoNumbers(num1, num2){
+  console.log(num1+num2)
 }
+
+
+// prueba addTwoNumbers(2,3)
