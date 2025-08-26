40712c25cc7863ffd1bdbf7c00ff50c67e35d5de
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-08-26 10:34:31 -0600

    se implemento una nueva funcion que resta dos numeros

--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ export function addTwoNumbers(num1, num2){
   console.log(num1+num2)
 }
 
-
 // prueba addTwoNumbers(2,3)
+
+// Resta de dos numeros
+
+function restTwoNumbers(num1, num2){
+  console.log(num1-num2)
+}
+
+// prueba de resta
+
+//restTwoNumbers(4,1)
+
